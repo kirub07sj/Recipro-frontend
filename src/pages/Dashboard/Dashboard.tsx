5f403f144a053285ec8ebdd561da213d551f4060
kirub07sj <alpharomio67@gmail.com>
2026-05-24 01:45:41 +0300

    Fix: Re-restore recentlyViewedService and update Dashboard.tsx

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -37,8 +37,8 @@ const Dashboard = () => {
             fetchTodayIntake(userId).catch(console.error);
             setLoading(true);
             const promises = [
-                getRecentlyViewedService(userId, 20).then(res => {
-                    if (res.success) setRecentlyViewed(res.data); 
+                getRecentlyViewedService(userId, 20).then((res: { success: any; data: any[]; }) => {
+                    if (res.success) setRecentlyViewed(res.data);
                 }),
                 profile ? Promise.resolve() : fetchProfile(userId),
                 fetchTodayIntake(userId)
